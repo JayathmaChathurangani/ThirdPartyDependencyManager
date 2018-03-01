0cd464fdde33518fd0310ba360dea3f725cabe00
Jayathma Chathurangani <31263300+JayathmaChathurangani@users.noreply.github.com>
2018-03-01 20:50:54 +0530

    Update ReaderYaml.java

--- Services/cronjobs/src/main/java/org/wso2/internalapps/lm/thirdpartylibrary/common/filereader/ReaderYaml.java
+++ Services/cronjobs/src/main/java/org/wso2/internalapps/lm/thirdpartylibrary/common/filereader/ReaderYaml.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 public class ReaderYaml {
     public static POJO_Config_File readYaml() throws IOException {
 
-        com.esotericsoftware.yamlbeans.YamlReader reader = new com.esotericsoftware.yamlbeans.YamlReader(new FileReader("/home/jayathma/WSO2_Intern_Dashboard/JavaCron/src/main/resources/LM_DB_Configs.yml"));
+        com.esotericsoftware.yamlbeans.YamlReader reader = new com.esotericsoftware.yamlbeans.YamlReader(new FileReader("/config/LM_DB_Configs.yml"));
 
         POJO_Config_File remoteRepoConfigurations= reader.read(POJO_Config_File.class);
 
